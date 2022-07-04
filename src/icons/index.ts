cc68d8a1491626e5a2ded8d0e94fb7495f9928c1
Aman Harwara <amanharwara@protonmail.com>
2022-07-04 13:29:31 +0530

    feat: add new icons

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,7 +1,17 @@
-import HamburgerMenu from "../icons/hamburger-menu.svg";
+import HamburgerMenuIcon from "../icons/hamburger-menu.svg";
+import DownloadIcon from "../icons/download.svg";
+import ImportExportIcon from "../icons/import-export.svg";
+import SponsorIcon from "../icons/sponsor.svg";
+import SettingsIcon from "../icons/settings.svg";
+import BugsIcon from "../icons/bugs.svg";
 
 export const Icons = {
-  "hamburger-menu": HamburgerMenu,
+  "hamburger-menu": HamburgerMenuIcon,
+  download: DownloadIcon,
+  "import-export": ImportExportIcon,
+  sponsor: SponsorIcon,
+  settings: SettingsIcon,
+  bugs: BugsIcon,
 };
 
 export type IconType = keyof typeof Icons;
