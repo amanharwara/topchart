4986274a34e0f143fca84a14f27cf28941294c42
Aman Harwara <amanharwara@protonmail.com>
2022-07-23 20:48:06 +0000

    feat: improve modal component

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,34 +1,50 @@
-import { Component, createSignal, JSX, Show } from "solid-js";
+import {
+  Component,
+  createEffect,
+  JSX,
+  onCleanup,
+  onMount,
+  Show,
+} from "solid-js";
 import { Portal } from "solid-js/web";
 import CloseIcon from "./icons/CloseIcon";
 
 type Props = {
   children: JSX.Element;
-  isOpen?: boolean;
-  onClose?: () => void;
   title?: string;
+  isOpen: boolean;
+  closeModal: () => void;
 };
 
 const Modal: Component<Props> = (props) => {
-  const [isOpenSignal, setIsOpenSignal] = createSignal(false);
-  const isOpen = () =>
-    typeof props.isOpen !== "undefined" ? props.isOpen : isOpenSignal();
+  let closeButtonRef: HTMLButtonElement | undefined;
 
-  const onClose = () => {
-    if (props.onClose) {
-      props.onClose();
-      return;
+  const closeOnEscape = (event: KeyboardEvent) => {
+    if (event.key === "Escape") {
+      props.closeModal();
     }
-
-    setIsOpenSignal(!isOpenSignal());
   };
 
+  createEffect(() => {
+    if (props.isOpen) {
+      closeButtonRef?.focus();
+    }
+  });
+
+  onMount(() => {
+    document.addEventListener("keydown", closeOnEscape);
+  });
+
+  onCleanup(() => {
+    document.removeEventListener("keydown", closeOnEscape);
+  });
+
   return (
-    <Show when={isOpen()}>
+    <Show when={props.isOpen}>
       <Portal>
         <div
           class="absolute top-0 left-0 h-full w-full bg-gray-900 opacity-60"
-          onClick={onClose}
+          onClick={props.closeModal}
         />
         <div class="absolute top-1/2 left-1/2 min-w-96 -translate-y-1/2 -translate-x-1/2 rounded-sm bg-slate-700 text-white">
           <div class="flex w-full items-center border-b border-slate-600">
@@ -37,7 +53,8 @@ const Modal: Component<Props> = (props) => {
             </div>
             <button
               class="flex items-center border-l border-slate-600 p-2.5 hover:bg-slate-600"
-              onClick={onClose}
+              onClick={props.closeModal}
+              ref={closeButtonRef}
             >
               <CloseIcon class="h-4 w-4 text-white" />
             </button>
