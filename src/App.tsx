5c7c8f6b73383a15bc6a35941016a5398bf495ac
Aman Harwara <amanharwara@protonmail.com>
2022-07-26 00:49:40 +0530

    feat: some chart options

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { createSignal } from "solid-js";
 import AddCoverArt from "./components/AddCoverArt";
 import AddCoverArtModal from "./components/AddCoverArtModal";
 import Button from "./components/Button";
+import ChartOptions from "./components/ChartOptions";
 import Header from "./components/Header";
 
 function App() {
@@ -11,7 +12,8 @@ function App() {
     <div class="flex h-full flex-col overflow-hidden">
       <Header />
       <div class="grid min-h-0 flex-grow grid-cols-[1fr_2.5fr_1fr]">
-        <div class="bg-gray-800">
+        <ChartOptions />
+        <div class="p-4">
           <Button
             onClick={() => toggleCoverArtModal(true)}
             hideLabelOnMobile={false}
@@ -19,7 +21,6 @@ function App() {
             Cover art modal
           </Button>
         </div>
-        <div class=""></div>
         <div class="flex min-h-0 flex-col bg-gray-800">
           <AddCoverArt />
         </div>
---
+++ src/components/ChartOptions.tsx
@@ -0,0 +1,98 @@
+import { Component } from "solid-js";
+import IconButton from "./IconButton";
+import ColorPickerIcon from "./icons/ColorPickerIcon";
+import EditIcon from "./icons/EditIcon";
+import HashIcon from "./icons/HashIcon";
+import LinkIcon from "./icons/LinkIcon";
+import TrashIcon from "./icons/TrashIcon";
+
+const ChartOptions: Component = () => {
+  return (
+    <div class="flex flex-col gap-6 bg-gray-800 py-4 px-5 text-white">
+      <div class="flex flex-col gap-2.5">
+        <div class="text-lg font-semibold">Current chart:</div>
+        <div class="flex gap-3">
+          <input
+            class="flex-grow rounded border border-slate-600 bg-transparent px-2.5 py-2 text-xs placeholder:text-slate-400"
+            placeholder="Select chart"
+            value="Untitled chart"
+          />
+          <IconButton icon={EditIcon} label="Edit chart title" />
+          <IconButton icon={TrashIcon} label="Delete chart" />
+        </div>
+      </div>
+      <div class="flex flex-col gap-2.5">
+        <div class="text-lg font-semibold">Chart type:</div>
+        <div class="flex gap-3">
+          <input
+            class="flex-grow rounded border border-slate-600 bg-transparent px-2.5 py-2 text-xs placeholder:text-slate-400"
+            placeholder="Select chart type"
+            value="Music Collage"
+          />
+        </div>
+      </div>
+      <div class="flex flex-col gap-2.5">
+        <div class="text-lg font-semibold">Rows:</div>
+        <div class="flex gap-3">
+          <input type="range" class="flex-grow " value={3} min={1} max={10} />
+          <input
+            type="number"
+            class="max-w-[4rem] rounded border border-slate-600 bg-transparent px-2.5 py-2 text-xs placeholder:text-slate-400"
+            value={3}
+          />
+        </div>
+      </div>
+      <div class="flex flex-col gap-2.5">
+        <div class="text-lg font-semibold">Columns:</div>
+        <div class="flex gap-3">
+          <input type="range" class="flex-grow " value={3} min={1} max={10} />
+          <input
+            type="number"
+            class="max-w-[4rem] rounded border border-slate-600 bg-transparent px-2.5 py-2 text-xs placeholder:text-slate-400"
+            value={3}
+          />
+        </div>
+      </div>
+      <div class="flex flex-col gap-2.5">
+        <div class="text-lg font-semibold">Background:</div>
+        <div class="flex rounded border border-slate-600">
+          <div class="flex items-center border-r border-slate-600 px-2.5 py-2">
+            <LinkIcon class="h-4 w-4" />
+          </div>
+          <input
+            class="flex-grow bg-transparent px-2.5 py-2 text-xs placeholder:text-slate-400"
+            placeholder="Enter image URL..."
+            value="https://somewebsite.com/image.png"
+          />
+        </div>
+      </div>
+      <div class="flex flex-col gap-2.5">
+        <div class="text-lg font-semibold">Font style:</div>
+        <input
+          class="flex-grow rounded border border-slate-600 bg-transparent px-2.5 py-2 text-xs placeholder:text-slate-400"
+          placeholder="Select font"
+          value="Inter"
+        />
+        <div class="flex flex-grow gap-1.5">
+          <div class="flex flex-grow rounded border border-slate-600">
+            <div class="flex items-center border-r border-slate-600 px-2.5 py-2">
+              <HashIcon class="h-4 w-4" />
+            </div>
+            <input
+              class="flex-grow bg-transparent px-2.5 py-2 text-xs placeholder:text-slate-400"
+              placeholder="Enter color..."
+              value="FFFFFF"
+            />
+          </div>
+          <IconButton
+            className="flex-shrink-0"
+            icon={ColorPickerIcon}
+            label="Pick color"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChartOptions;
