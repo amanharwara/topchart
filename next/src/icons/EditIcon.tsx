4706b6dacab76216ff8139301b8053595dacfad0
Aman Harwara <amanharwara@protonmail.com>
2022-08-17 16:07:08 +0530

    fix: react error

--- next/src/icons/EditIcon.tsx
+++ next/src/icons/EditIcon.tsx
@@ -8,10 +8,10 @@ const EditIcon = (props: ComponentPropsWithoutRef<"svg">) => (
     viewBox="0 0 24 24"
     {...props}
   >
-    <g fill="none" stroke="currentColor" stroke-width="2">
+    <g fill="none" stroke="currentColor" strokeWidth="2">
       <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         d="M4.333 16.048L16.57 3.81a2.56 2.56 0 0 1 3.62 3.619L7.951 19.667a2 2 0 0 1-1.022.547L3 21l.786-3.93a2 2 0 0 1 .547-1.022Z"
       />
       <path d="m14.5 6.5l3 3" />
