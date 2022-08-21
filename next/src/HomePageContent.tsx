598c5445c5fe1c64a053408b573e05600827626a
Aman Harwara <amanharwara@protonmail.com>
2022-08-21 14:16:42 +0530

    feat: disable ssr

---
+++ next/src/HomePageContent.tsx
@@ -0,0 +1,81 @@
+import { Button } from "ariakit";
+import AddCoverArt from "./addCoverArt/AddCoverArt";
+import ChartOptionsSection from "./chartOptions/ChartOptionsSection";
+import Header from "./header/Header";
+import MusicCollage from "./musicCollage/MusicCollage";
+import { useSetMusicCollageItem } from "./stores/charts";
+
+const HomePageContent = () => {
+  const setMusicCollageItem = useSetMusicCollageItem();
+
+  return (
+    <>
+      <Header />
+      <div className="grid min-h-0 flex-grow md:grid-cols-[1fr_2.5fr_1fr]">
+        <ChartOptionsSection />
+        <main className="overflow-auto p-4">
+          <div className="mb-4 text-white">
+            TODO: Rip out everything and start again with mobile-first markup
+          </div>
+          <MusicCollage />
+          <div className="mt-5 flex items-center gap-4 text-white">
+            DevTools:
+            <Button
+              onClick={() => {
+                setMusicCollageItem(0, {
+                  title: "Brian Eno",
+                  image: "eno.jpg",
+                });
+              }}
+            >
+              Add image to first
+            </Button>
+            {/* 
+        <Button
+          onClick={() => {
+            setCharts(
+              (chart) => selectedChart().id === chart.id,
+              "options",
+              "musicCollage",
+              "items",
+              (rows) =>
+                rows.map((row) =>
+                  row.map(() => ({
+                    title: "",
+                    image: null,
+                  }))
+                )
+            );
+          }}
+        >
+          Clear items
+        </Button>
+        <Button
+          onClick={() => {
+            setCharts(
+              (chart) => selectedChart().id === chart.id,
+              "options",
+              "musicCollage",
+              "items",
+              (items) => items.slice()
+            );
+          }}
+        >
+          Force refresh chart
+        </Button> */}
+          </div>
+        </main>
+        <section className="flex min-h-0 flex-col bg-gray-800">
+          <AddCoverArt />
+        </section>
+      </div>
+      {/* <AddCoverArtModal
+    isOpen={isCoverArtModalOpen()}
+    closeModal={() => toggleCoverArtModal(false)}
+  />
+  <ToastContainer /> */}
+    </>
+  );
+};
+
+export default HomePageContent;
--- next/src/pages/index.tsx
+++ next/src/pages/index.tsx
@@ -1,19 +1,12 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import AddCoverArt from "../addCoverArt/AddCoverArt";
-import ChartOptionsSection from "../chartOptions/ChartOptionsSection";
-import Header from "../header/Header";
-import Button from "../components/Button";
-import { useSetMusicCollageItem } from "../stores/charts";
 import dynamic from "next/dynamic";
+import Head from "next/head";
 
-const MusicCollage = dynamic(() => import("../musicCollage/MusicCollage"), {
+const DynamicContent = dynamic(() => import("../HomePageContent"), {
   ssr: false,
 });
 
 const Home: NextPage = () => {
-  const setMusicCollageItem = useSetMusicCollageItem();
-
   return (
     <>
       <Head>
@@ -28,70 +21,7 @@ const Home: NextPage = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <div className="flex flex-col overflow-hidden">
-        <Header />
-        <div className="grid min-h-0 flex-grow md:grid-cols-[1fr_2.5fr_1fr]">
-          <ChartOptionsSection />
-          <main className="overflow-auto p-4">
-            <div className="mb-4 text-white">
-              TODO: Rip out everything and start again with mobile-first markup
-            </div>
-            <MusicCollage />
-            <div className="mt-5 flex items-center gap-4 text-white">
-              DevTools:
-              <Button
-                onClick={() => {
-                  setMusicCollageItem(0, {
-                    title: "Brian Eno",
-                    image: "eno.jpg",
-                  });
-                }}
-              >
-                Add image to first
-              </Button>
-              {/* 
-              <Button
-                onClick={() => {
-                  setCharts(
-                    (chart) => selectedChart().id === chart.id,
-                    "options",
-                    "musicCollage",
-                    "items",
-                    (rows) =>
-                      rows.map((row) =>
-                        row.map(() => ({
-                          title: "",
-                          image: null,
-                        }))
-                      )
-                  );
-                }}
-              >
-                Clear items
-              </Button>
-              <Button
-                onClick={() => {
-                  setCharts(
-                    (chart) => selectedChart().id === chart.id,
-                    "options",
-                    "musicCollage",
-                    "items",
-                    (items) => items.slice()
-                  );
-                }}
-              >
-                Force refresh chart
-              </Button> */}
-            </div>
-          </main>
-          <section className="flex min-h-0 flex-col bg-gray-800">
-            <AddCoverArt />
-          </section>
-        </div>
-        {/* <AddCoverArtModal
-          isOpen={isCoverArtModalOpen()}
-          closeModal={() => toggleCoverArtModal(false)}
-        />
-        <ToastContainer /> */}
+        <DynamicContent />
       </div>
     </>
   );
