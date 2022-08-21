355a34f2d18c28ce0503803ac80b1fcf523b2e6d
Aman Harwara <amanharwara@protonmail.com>
2022-08-21 21:47:13 +0530

    feat: chart selection & title editing

--- next/src/chartOptions/ChartOptionsSection.tsx
+++ next/src/chartOptions/ChartOptionsSection.tsx
@@ -1,17 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import IconButton from "../components/IconButton";
 import Select from "../components/Select";
 import AddIcon from "../icons/AddIcon";
 import EditIcon from "../icons/EditIcon";
 import SaveIcon from "../icons/SaveIcon";
 import TrashIcon from "../icons/TrashIcon";
+import {
+  useAddChart,
+  useChartsList,
+  useSelectedChart,
+  useSetSelectedChartId,
+  useSetSelectedChartTitle,
+} from "../stores/charts";
 import MusicCollageOptions from "./MusicCollageOptions";
 
 const CurrentChartOption = () => {
-  const [selectedChart, setSelectedChart] = useState<string>();
+  const selectedChart = useSelectedChart();
+  const setSelectedChartId = useSetSelectedChartId();
+  const setSelectedChartTitle = useSetSelectedChartTitle();
+  const charts = useChartsList();
+  const addNewChart = useAddChart();
 
   const [isEditingChart, setIsEditingChart] = useState(false);
   const isSelectingChart = !isEditingChart;
+
+  const [currentTitle, setCurrentTitle] = useState(selectedChart?.title);
+
+  useEffect(() => {
+    setCurrentTitle(selectedChart?.title);
+  }, [selectedChart]);
 
   return (
     <div className="flex flex-col gap-2.5">
@@ -20,36 +37,21 @@ const CurrentChartOption = () => {
         {isSelectingChart && (
           <>
             <Select
-              value={selectedChart}
-              setValue={setSelectedChart}
-              options={[
-                {
-                  label: "test",
-                  value: "test",
-                },
-                {
-                  label: "test2",
-                  value: "test2",
-                },
-                {
-                  label: "test3",
-                  value: "test3",
-                },
-                {
-                  label: "test4",
-                  value: "test4",
-                },
-              ]}
+              value={selectedChart!.id}
+              setValue={setSelectedChartId}
+              options={charts.map(({ id, title }) => ({
+                value: id,
+                label: title,
+              }))}
             />
             <IconButton
               className="px-2.5"
               icon={AddIcon}
               label="Add new chart"
-              // onClick={() => {
-              //   const id = addNewChart();
-
-              //   setSelectedChart(charts.find((chart) => chart.id === id));
-              // }}
+              onClick={() => {
+                const id = addNewChart();
+                setSelectedChartId(id);
+              }}
             />
             <IconButton
               className="px-2.5"
@@ -63,38 +65,52 @@ const CurrentChartOption = () => {
               className="px-2.5"
               icon={TrashIcon}
               label="Delete chart"
-              // onClick={() => {
-              //   const shouldDeleteChart = confirm(
-              //     `Do you want to delete chart "${selectedChart().title}"?`
-              //   );
+              onClick={() => {
+                const shouldDeleteChart = confirm(
+                  `Do you want to delete chart "${selectedChart!.title}"?`
+                );
 
-              //   if (shouldDeleteChart) {
-              //     deleteChart(selectedChart().id);
-              //   }
-              // }}
+                /* if (shouldDeleteChart) {
+                  deleteChart(selectedChart().id);
+                } */
+              }}
             />
           </>
         )}
         {isEditingChart && (
-          <>
+          <form
+            className="flex flex-grow gap-2"
+            onSubmit={(event) => {
+              event.preventDefault();
+
+              if (!currentTitle) {
+                return;
+              }
+
+              setSelectedChartTitle(currentTitle);
+              setIsEditingChart(false);
+            }}
+          >
             <input
               className="flex-grow rounded border border-slate-600 bg-transparent px-2.5 py-2 text-sm placeholder:text-slate-400"
               placeholder="Enter chart title..."
-              // value={currentTitle()}
-              // onInput={(event) => {
-              //   setCurrentTitle(event.currentTarget.value);
-              // }}
+              value={currentTitle}
+              onInput={(event) => {
+                setCurrentTitle(event.currentTarget.value);
+              }}
+              ref={(element) => {
+                if (document.activeElement !== element) {
+                  element?.focus();
+                }
+              }}
             />
             <IconButton
+              type="submit"
               className="px-2.5"
               icon={SaveIcon}
               label={"Save chart title"}
-              onClick={() => {
-                // editChartTitle(selectedChart().id, currentTitle());
-                setIsEditingChart(false);
-              }}
             />
-          </>
+          </form>
         )}
       </div>
     </div>
