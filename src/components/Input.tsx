edcd1756617ce3ec3c8a2f6bc5f8d870ed9545ef
Aman Harwara <amanharwara@protonmail.com>
2022-08-26 02:40:11 +0530

    feat: disabled state

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,14 +1,24 @@
-import { ComponentPropsWithoutRef } from "react";
+import { ComponentPropsWithoutRef, ForwardedRef, forwardRef } from "react";
 import classNames from "../utils/classNames";
 
-const Input = ({ className, ...props }: ComponentPropsWithoutRef<"input">) => (
-  <input
-    {...props}
-    className={classNames(
-      "flex-grow rounded border border-slate-600 bg-transparent px-2.5 py-2 text-sm placeholder:text-slate-400",
-      className
-    )}
-  />
-);
+const Input = forwardRef(function Input(
+  { className, disabled, ...props }: ComponentPropsWithoutRef<"input">,
+  ref: ForwardedRef<HTMLInputElement>
+) {
+  return (
+    <input
+      {...props}
+      disabled={disabled}
+      className={classNames(
+        "flex-grow rounded border border-slate-600 bg-transparent px-2.5 py-2 text-sm",
+        disabled
+          ? "cursor-not-allowed border-gray-500 text-gray-400 placeholder:text-gray-400"
+          : "border-slate-600 text-white placeholder:text-slate-400",
+        className
+      )}
+      ref={ref}
+    />
+  );
+});
 
 export default Input;
