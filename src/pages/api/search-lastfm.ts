177ee42b535cc58962ce223a6b3f7946b8bb2d96
Aman Harwara <amanharwara@protonmail.com>
2022-11-05 23:37:51 +0530

    feat: add api response caching

--- src/pages/api/search-lastfm.ts
+++ src/pages/api/search-lastfm.ts
@@ -1,12 +1,24 @@
-import { type NextRequest } from "next/server";
+import type { NextApiRequest, NextApiResponse } from "next";
 
-export const config = {
-  runtime: "experimental-edge",
-};
+const TwoDaysInSeconds = 60 * 60 * 24 * 2;
 
-export default async function handler(request: NextRequest) {
-  const { query } = await request.json();
-  return fetch(
-    `https://ws.audioscrobbler.com/2.0/?method=album.search&album=${query}&limit=25&api_key=${process.env.LASTFM_API_KEY}&format=json`
-  );
+export default async function handler(
+  request: NextApiRequest,
+  response: NextApiResponse
+) {
+  response.setHeader("Cache-Control", `s-maxage=${TwoDaysInSeconds}`);
+
+  const { query } = request.body;
+
+  if (!query) {
+    response.status(400).json({ error: "Missing query" });
+  }
+
+  const lastFmResponse = await (
+    await fetch(
+      `https://ws.audioscrobbler.com/2.0/?method=album.search&album=${query}&limit=25&api_key=${process.env.LASTFM_API_KEY}&format=json`
+    )
+  ).json();
+
+  return response.status(200).json(lastFmResponse);
 }
