5b49a8eb2959edcc6c8a5122a1f0865336a6598b
Aman Harwara <amanharwara@protonmail.com>
2022-11-06 02:21:54 +0530

    feat: spinner width

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,6 +1,18 @@
-const Spinner = ({ className }: { className?: string }) => (
+import classNames from "../utils/classNames";
+
+const Spinner = ({
+  className,
+  width = 1,
+}: {
+  className?: string;
+  width?: 1 | 2 | 4 | 8;
+}) => (
   <div
-    className={`animate-spin rounded-full border border-solid border-info border-r-transparent ${className}`}
+    className={classNames(
+      "animate-spin rounded-full border-solid border-info border-r-transparent",
+      width === 1 ? "border" : `border-${width}`,
+      className
+    )}
   />
 );
 
