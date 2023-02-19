eefcdba8d3ae64b88f3369ae8a07c4c886180d23
Aman Harwara <amanharwara@protonmail.com>
2023-02-19 21:53:09 +0530

    fix: header on mobile

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -21,7 +21,9 @@ const Tooltip = ({
 
   return (
     <>
-      <TooltipAnchor state={state}>{children}</TooltipAnchor>
+      <TooltipAnchor className="hidden md:block" state={state}>
+        {children}
+      </TooltipAnchor>
       <AriakitTooltip
         state={state}
         className="dark:bg-slate-600 dark:text-white bg-slate-100 py-1 px-2.5 rounded border border-gray-800 dark:border-0"
