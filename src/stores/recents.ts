e58db593f93dc0c0335a69d8d97684f0e46c05a9
Aman Harwara <amanharwara@protonmail.com>
2023-03-02 00:48:08 +0530

    chore: upgrade stuff

--- src/stores/recents.ts
+++ src/stores/recents.ts
@@ -9,27 +9,31 @@ type RecentsStore = {
 };
 
 export const recentsStore = createStore<RecentsStore>()(
-  persist((set) => ({
-    recentlyUploadedImages: [],
-    addRecentlyUploadedImage: (id: string) => {
-      set(
-        produce((state: RecentsStore) => {
-          if (state.recentlyUploadedImages.length === 5) {
-            state.recentlyUploadedImages.shift();
-          }
-          if (state.recentlyUploadedImages.includes(id)) {
-            state.recentlyUploadedImages = state.recentlyUploadedImages.filter(
-              (s) => s !== id
-            );
-          }
-          state.recentlyUploadedImages.push(id);
-        })
-      );
-    },
-    clearRecentlyUploadedImages: () => {
-      set({
-        recentlyUploadedImages: [],
-      });
-    },
-  }))
+  persist(
+    (set) => ({
+      recentlyUploadedImages: [],
+      addRecentlyUploadedImage: (id: string) => {
+        set(
+          produce((state: RecentsStore) => {
+            if (state.recentlyUploadedImages.length === 5) {
+              state.recentlyUploadedImages.shift();
+            }
+            if (state.recentlyUploadedImages.includes(id)) {
+              state.recentlyUploadedImages =
+                state.recentlyUploadedImages.filter((s) => s !== id);
+            }
+            state.recentlyUploadedImages.push(id);
+          })
+        );
+      },
+      clearRecentlyUploadedImages: () => {
+        set({
+          recentlyUploadedImages: [],
+        });
+      },
+    }),
+    {
+      name: "recents",
+    }
+  )
 );
--- src/stores/settings.ts
+++ src/stores/settings.ts
@@ -1,4 +1,4 @@
-import create from "zustand";
+import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
 interface SettingsStore {
@@ -20,7 +20,6 @@ const useSettingsStore = create<SettingsStore>()(
     }),
     {
       name: "settings",
-      getStorage: () => localStorage,
       partialize: (state) =>
         Object.fromEntries(
           Object.entries(state).filter(
